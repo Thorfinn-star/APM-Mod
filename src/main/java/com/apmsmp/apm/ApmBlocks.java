378c2f6ad643ee5f2a5e7d200c3c27c594ede176
Thorfinn-star <diegotommasi2009@gmail.com>
2026-09-16 21:56:13 +0200

    Add APM ore and storage block

---
+++ src/main/java/com/apmsmp/apm/ApmBlocks.java
@@ -0,0 +1,37 @@
+package com.apmsmp.apm;
+
+import java.util.function.Function;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.references.BlockItemId;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public final class ApmBlocks {
+    public static final Block APM_ORE = register("apm_ore", Block::new, BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_DIAMOND_ORE).strength(5.0F, 7.0F).sound(SoundType.DEEPSLATE));
+    public static final Block APM_BLOCK = register("apm_block", Block::new, BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK).strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK));
+
+    private static Block register(String name, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties props) {
+        Identifier id = ApmMod.id(name);
+        BlockItemId ids = BlockItemId.create(id, id);
+        ResourceKey<Block> blockKey = ids.block();
+        Block block = factory.apply(props.setId(blockKey));
+        Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
+        BlockItem item = new BlockItem(block, new Item.Properties().useBlockDescriptionPrefix().setId(ids.item()));
+        Registry.register(BuiltInRegistries.ITEM, ids.item(), item);
+        return block;
+    }
+    public static void initialize() {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.NATURAL_BLOCKS).register(out -> out.accept(APM_ORE.asItem()));
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.BUILDING_BLOCKS).register(out -> out.accept(APM_BLOCK.asItem()));
+    }
+    private ApmBlocks() {}
+}
